6bd230a1fb8802246aeea64b6cdd773444407521
Leah Fenster <leahefen@gmail.com>
2024-08-09 14:53:52 +0100

    styled navbar and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,26 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <NavLink
-          to="/"
-          //   className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          Home
-        </NavLink>
-        <br />
-        <NavLink to="/animalList">Animals</NavLink>
-        <br />
-        <NavLink to="/map">Map</NavLink>
-        <br />
-        <NavLink to="/add">Add a Sighting</NavLink>
-        <br />
-        <NavLink to="/watchList">View your Watchlist</NavLink>
-      </ul>
-    </nav>
+    <div className="navbar">
+      <nav>
+        <ul>
+          <NavLink
+            to="/"
+            //   className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Home
+          </NavLink>
+          <br />
+          <NavLink to="/animalList">Animals</NavLink>
+          <br />
+          <NavLink to="/map">Map</NavLink>
+          <br />
+          <NavLink to="/add">Add a Sighting</NavLink>
+          <br />
+          <NavLink to="/watchList">View your Watchlist</NavLink>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
