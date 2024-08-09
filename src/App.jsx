6cb88dc3f6a6a05b60d6daf9949d8d2aacb0fedf
smammc <smammc@hotmail.com>
2024-08-09 15:46:42 +0100

    imported navbar and footer to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   const [count, setCount] = useState(0);
 
