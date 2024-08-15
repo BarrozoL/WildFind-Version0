f296948b820a440ca1f2e9019d1a260051641a86
BarrozoL <barrozo.lucasp@gmail.com>
2024-08-15 11:09:42 +0100

    adding location name to map

--- src/pages/MapListing.jsx
+++ src/pages/MapListing.jsx
@@ -1,15 +1,29 @@
 import { Link } from "react-router-dom";
 
-export default function MapListing({ matchingAnimals }) {
+export default function MapListing({ matchingAnimals, location }) {
   const animalsArray = Array.from(matchingAnimals);
+
+  /* const firstSightingLocation = animalsArray.find((animal) => {
+    animal.sightings && animal.sightings[0] && animal.sightings[0].location;
+  }); */
 
   return (
     <>
       <div className="map-sightings">
+        <div>
+          {" "}
+          {location.length > 0 ? (
+            <h2 className="location-tag">
+              Animals with sightings in: {location}
+            </h2>
+          ) : (
+            ""
+          )}
+        </div>
         {animalsArray.map((animal) => (
           <Link to={`/animal-list/${animal.id}`} key={animal.id}>
             <div>
-              <h4 style={{ color: "green" }}>{animal.name}</h4>
+              <h4 style={{ color: "green", width: "100px" }}>{animal.name}</h4>
               <img stylewidth="100px" src={animal.image} />
             </div>
           </Link>
