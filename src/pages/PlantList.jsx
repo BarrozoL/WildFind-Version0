cf41babe79b324947ed3268be54c8df83b6c5e3d
BarrozoL <barrozo.lucasp@gmail.com>
2024-08-15 17:49:35 +0100

    image size

--- src/pages/PlantList.jsx
+++ src/pages/PlantList.jsx
@@ -9,7 +9,7 @@ export default function PlantList({ plants }) {
             <Link to={`/plant-list/${plant.id}`}>
               <div className="animal-cards" key={plant.id}>
                 <h3 style={{ color: "rgb(44,140,121)" }}>{plant.name}</h3>
-                <img src={plant.image} />
+                <img width="180px" height="150px;" src={plant.image} />
               </div>
             </Link>
           );
