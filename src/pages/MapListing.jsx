086f71db6e4cbe36d950cfca82826ab15b3e6e13
BarrozoL <barrozo.lucasp@gmail.com>
2024-08-16 08:14:19 +0100

    adding plants to map

--- src/pages/MapListing.jsx
+++ src/pages/MapListing.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function MapListing({ matchingAnimals, location }) {
+export default function MapListing({
+  matchingAnimals,
+  matchingPlants,
+  location,
+}) {
   const animalsArray = Array.from(matchingAnimals);
+
+  const plantsArray = Array.from(matchingPlants);
 
   /* const firstSightingLocation = animalsArray.find((animal) => {
     animal.sightings && animal.sightings[0] && animal.sightings[0].location;
@@ -13,9 +19,7 @@ export default function MapListing({ matchingAnimals, location }) {
         <div>
           {" "}
           {location.length > 0 ? (
-            <h2 className="location-tag">
-              Animals with sightings in: {location}
-            </h2>
+            <h2 className="location-tag">Sightings in: {location}</h2>
           ) : (
             ""
           )}
@@ -27,10 +31,24 @@ export default function MapListing({ matchingAnimals, location }) {
                 <h4 style={{ color: "green", width: "100px" }}>
                   {animal.name}
                 </h4>
-                <img stylewidth="100px" src={animal.image} />
+                <img width="100px" src={animal.image} />
               </div>
             </Link>
           ))}
+        </div>
+        <div>
+          <div style={{ position: "absolute", top: "60%" }}>
+            {plantsArray.map((plant) => (
+              <Link to={`/plant-list/${plant.id}`} key={plant.id}>
+                <div>
+                  <h4 style={{ color: "green", width: "100px" }}>
+                    {plant.name}
+                  </h4>
+                  <img width="100px" src={plant.image} />
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </>
--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -4,19 +4,36 @@ import "./Map.css";
 import Map from "../components/Map";
 import MapListing from "./MapListing";
 
-export default function MapPage({ getAnimalsWithSightings }) {
+export default function MapPage({
+  getAnimalsWithSightings,
+  getPlantsWithSightings,
+}) {
   const [location, setLocation] = useState([]);
   const [animalSightings, setAnimalSightings] = useState([]);
+  const [plantSightings, setPlantSightings] = useState([]);
 
   useEffect(() => {
     getAnimalsWithSightings().then((data) => setAnimalSightings(data));
+  }, [setLocation]);
+
+  useEffect(() => {
+    getPlantsWithSightings().then((data) => setPlantSightings(data));
   }, [setLocation]);
 
   function findLocation(e) {
     setLocation(e.target.id);
   }
 
-  /*  let matchingAnimals = []; */
+  let matchingPlants = new Set([]);
+
+  plantSightings.map((plant) => {
+    plant.sightings.map((sight) => {
+      if (location === sight.location) {
+        console.log("this is a plant", plant, matchingPlants);
+        matchingPlants.add(plant);
+      }
+    });
+  });
 
   let matchingAnimals = new Set([]);
 
@@ -31,7 +48,11 @@ export default function MapPage({ getAnimalsWithSightings }) {
   return (
     <>
       <Map findLocation={findLocation} />
-      <MapListing location={location} matchingAnimals={matchingAnimals} />
+      <MapListing
+        location={location}
+        matchingAnimals={matchingAnimals}
+        matchingPlants={matchingPlants}
+      />
     </>
   );
 }
